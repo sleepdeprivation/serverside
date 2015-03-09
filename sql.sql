30043a6118aea8c1bdf7fb36a0de2e3ecfe12b95
sleepdeprivation <sleepdeprivation@users.noreply.github.com>
2015-03-09 11:19:42 -0700

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,2 +1,2 @@
 create table User (uid int primary key auto_increment, username varchar(32), password varchar(32));
-create table Message (mid int primary key auto_increment, content varchar(1024), 
+create table Message (mid int primary key auto_increment, content varchar(1024), lat float(10,6), lng float(10,6));
