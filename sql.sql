24a675d670d2f2a474964e4f323ca4e8bcbd78f7
sleepdeprivation <sleepdeprivation@users.noreply.github.com>
2015-03-09 11:17:02 -0700

    Create sql.sql

---
+++ sql.sql
@@ -0,0 +1,2 @@
+create table User (uid int primary key auto_increment, username varchar(32), password varchar(32));
+create table Message (mid int primary key auto_increment, content varchar(1024), 
