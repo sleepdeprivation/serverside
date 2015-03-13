088d9e7d2510447dedf7e0856b137c38b7f57b10
sleepdeprivation <sleepdeprivation@users.noreply.github.com>
2015-03-12 22:50:52 -0700

    Update and rename sql.sql to schema.sql
    I needed to look at the Schema to see how the models would be made and decide to fix and test it.
    I unilaterally made some stylistic choices here as well.
    And I renamed the file. 
    Tested in MariaDB mysql database

---
+++ schema.sql
@@ -0,0 +1,18 @@
+Drop table if exists User;
+Drop table if exists Message;
+
+create table User 		(
+							userID int primary key auto_increment,
+							username varchar(32) unique not null,
+							password varchar(32) not null
+						);
+
+create table Message	(
+							messageID int primary key auto_increment,
+							posterID int references User(userID),
+							content varchar(1024),
+							lat float(10,6),
+							lon float(10,6),
+							numUpvotes int,
+							numDownvotes int
+						); 
--- sql.sql
+++
@@ -1,2 +0,0 @@
-create table User (uid int primary key auto_increment, username varchar(32) unique not null, password varchar(32) not null);
-create table Message (mid int primary key auto_increment, content varchar(1024), lat float(10,6), lon float(10,6));
