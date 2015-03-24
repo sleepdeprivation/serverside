8213e492d9d9496bfd618426767d1ccb092a5b86
sleepdeprivation <sleepdeprivation@users.noreply.github.com>
2015-03-24 14:49:59 -0700

    Update schema.sql
    Spit into two tables, with explanation as to why
    Left the drop table for the old fused message table so it will clean up the old one when you stick it in your database
    Also put some inserts for garbage data for testing purposes

--- schema.sql
+++ schema.sql
@@ -1,5 +1,8 @@
 Drop table if exists User;
 Drop table if exists Message;
+Drop table if exists HeadMessage;
+Drop table if exists ReplyMessage;
+
 
 create table User 		(
 							userID int primary key auto_increment,
@@ -7,13 +10,54 @@ create table User 		(
 							password varchar(32) not null
 						);
 
-create table Message	(
+#after reading about GSON, separated into 2 message types for easiness
+#while these may be redundant and essentially the same, only OP should have a lat/lon
+#and only replies should have a parent
+#if this proves to be a problem we can migrate later fairly easily I think
+create table HeadMessage	(
 							messageID int not null primary key auto_increment,
 							posterID int not null references User(userID),
-							parentID int default null references Message(messageID),
 							content varchar(1024),
 							lat float(10,6),
 							lon float(10,6),
 							numUpvotes int default 0,
 							numDownvotes int default 0
-						); 
+						);
+
+create table ReplyMessage(
+							messageID int not null primary key auto_increment,
+							posterID int not null references User(userID),
+							parentID int not null references HeadMessage(messageID),
+							content varchar(1024),
+							numUpvotes int default 0,
+							numDownvotes int default 0
+						);
+
+#insert garbage data
+insert into User (username, password) values ("user1", "hashedPass");
+insert into User (username, password) values ("user2", "hashedPass");
+insert into User (username, password) values ("user3", "hashedPass");
+insert into User (username, password) values ("user4", "hashedPass");
+insert into User (username, password) values ("user5", "hashedPass");
+
+insert into HeadMessage(posterID, content, lat, lon, numUpVotes, numDownVotes)
+	values(1, "OP message 1", 31, 121, 30, 10);
+insert into HeadMessage(posterID, content, lat, lon, numUpVotes, numDownVotes)
+	values(2, "OP message 2", 40, 120, 2, 5);
+insert into HeadMessage(posterID, content, lat, lon, numUpVotes, numDownVotes)
+	values(3, "OP message 3", 50, 120, 40, 66);
+insert into HeadMessage(posterID, content, lat, lon, numUpVotes, numDownVotes)
+	values(4, "OP message 4", 30, 110, 90, 1);
+
+insert into ReplyMessage(posterID, parentID, content, numUpVotes, numDownVotes)
+	values(1, 1, "reply message 1-1", 12, 7);
+insert into ReplyMessage(posterID, parentID, content, numUpVotes, numDownVotes)
+	values(1, 1, "reply message 1-2", 59, 0);
+insert into ReplyMessage(posterID, parentID, content, numUpVotes, numDownVotes)
+	values(1, 3, "reply message 3-1", 12, 2);
+insert into ReplyMessage(posterID, parentID, content, numUpVotes, numDownVotes)
+	values(1, 3, "reply message 1-2", 34, 800);
+insert into ReplyMessage(posterID, parentID, content, numUpVotes, numDownVotes)
+	values(1, 89, "reply message 1-1", 63, 9);
+
+
