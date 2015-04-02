fbf31df7a3fb01201ec3f975e24f969538b6b888
Derek Warren <dwarren4096@gmail.com>
2015-04-02 16:46:57 -0700

    added parseFloat()

--- server.js
+++ server.js
@@ -137,10 +137,10 @@ app.post('/submit/newop', function(req, res){
 */
 
 app.get('/getPostsByRange', function(req, res){
-	var 	latMin = req.query.latMin,
-		lonMin = req.query.lonMin,
-		latMax = req.query.latMax,
-		lonMax = req.query.lonMax;
+	var 	latMin = parseFloat(req.query.latMin),
+		lonMin = parseFloat(req.query.lonMin),
+		latMax = parseFloat(req.query.latMax),
+		lonMax = parseFloat(req.query.lonMax);
 	console.log('latMin='+latMin+'\n'+
 		'lonMin='+lonMin+'\n'+
 		'latMax='+latMax+'\n'+
